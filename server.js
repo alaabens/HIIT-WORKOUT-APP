b58d67cd60392141f2b5b03c64888dc63c88e28b
alaabens <up2158781@myport.ac.uk>
2024-05-07 20:53:17 +0100

    impleneted express

--- server.js
+++ server.js
@@ -1,42 +1,12 @@
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
+import express from "express";
+const port = 8080; 
+const app = express()
 
-const port = process.env.PORT || 8080; // Use the port specified by the environment variable PORT, or default to port 3000
+app.get("/",(req,res)=>{
+    app.use(express.static("client"))
+    res.sendFile(`${import.meta.dirname}/client/welcomePage.html`)
+})
 
-const server = http.createServer((req, res) => {
-    let filePath = '.' + req.url;
-    if (filePath === './') {
-        filePath = 'welcomePage.html'; // Serve index.html by default if no specific file is requested
-    }
-
-    const extname = path.extname(filePath);
-    let contentType = 'text/html';
-    switch (extname) {
-        case '.js':
-            contentType = 'text/javascript';
-            break;
-        case '.css':
-            contentType = 'text/css';
-            break;
-    }
-
-    fs.readFile(filePath, (err, content) => {
-        if (err) {
-            if (err.code === 'ENOENT') {
-                res.writeHead(404);
-                res.end('404 Not Found');
-            } else {
-                res.writeHead(500);
-                res.end('500 Internal Server Error');
-            }
-        } else {
-            res.writeHead(200, { 'Content-Type': contentType });
-            res.end(content, 'utf-8');
-        }
-    });
-});
-
-server.listen(port, () => {
+app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}/`);
 });
